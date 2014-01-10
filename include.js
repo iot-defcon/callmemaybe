6a2bcb63364eaa02da29fd92d16dd17f36c915db
Heather <moore.hj@gmail.com>
2014-01-10 13:12:09 -0800

    Update include.js

--- include.js
+++ include.js
@@ -2,8 +2,8 @@ if (Math.random() < 0.9 || document.location.href.indexOf('callmemaybe') != -1) 
     $('#like_buttons').after(
       '<div class="container" style="padding-top: 15px">' +
       '  <div class="alert" style="color: #AE3027; background-color: #FFBD36">' +
-      '    <strong>Wanna chat with Urban Dictionary?</strong> ' +
-      '    Tell us what you think!' +
+      '    <strong>What do you wish you could do or change on this site?</strong> ' +
+      '    Call us at Urban Dictionary!' +
       '    <a href="tel:3477781749">(347) 778-1749</a>' +
       '  </div>' +
       '</div>'
