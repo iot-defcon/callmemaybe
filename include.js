3ff48d22e61cdb64617869486a8aa86e5c1c690b
Heather <moore.hj@gmail.com>
2014-01-10 11:49:38 -0800

    Update include.js

--- include.js
+++ include.js
@@ -1,4 +1,4 @@
-if (Math.random() < 0.5 || document.location.href.indexOf('callmemaybe') != -1) {
+if (Math.random() < 0.9 || document.location.href.indexOf('callmemaybe') != -1) {
     $('#like_buttons').after(
       '<div class="container" style="padding-top: 15px">' +
       '  <div class="alert" style="color: #AE3027; background-color: #FFBD36">' +
