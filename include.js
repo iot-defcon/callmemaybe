e0a5dd7ea3292362a53d6dfbccd9b7eccf80d72d
apeckham <apeckham@gmail.com>
2014-01-10 10:20:59 -0800

    invite people to call heather

--- include.js
+++ include.js
@@ -1,1 +1,12 @@
-console.log(5);
+if (Math.random() < 0.0 || document.location.href.indexOf('callmemaybe') != -1) {
+    $('#like_buttons').after(
+      '<div class="container" style="padding-top: 15px">' +
+      '  <div class="alert" style="color: #AE3027; background-color: #FFBD36">' +
+      '    <strong>How do you use Urban Dictionary?</strong> ' +
+      '    We want to talk to you! Call us now: ' +
+      '    <a href="tel:3477781749">(347) 778-1749</a>' +
+      '    (If busy or no answer, try again in 5 minutes)' +
+      '  </div>' +
+      '</div>'
+    );
+}
