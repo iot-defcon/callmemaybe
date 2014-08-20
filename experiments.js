a548f3f3cd7559f5910350bc6ad5e12a98e1c9a2
apeckham <apeckham@gmail.com>
2014-08-20 11:00:34 -0700

    reformat

--- experiments.js
+++ experiments.js
@@ -1,16 +1,18 @@
 var pageVariations = [
-  function() {},
-  function() {
-    var newClass = document.body.getAttribute('class') + ' variation1';
-    document.body.setAttribute('class', newClass);
-  }
+    function () {
+        console.log($);
+    },
+    function () {
+        var newClass = document.body.getAttribute('class') + ' variation1';
+        document.body.setAttribute('class', newClass);
+    }
 ];
 
 var experiment = 'A6baa2IYRrWkqeVgndseyA';
 var variation = cxApi.getChosenVariation(experiment);
 if (variation === undefined) {
-  var variation = Math.floor(Math.random() * 1000000000) % pageVariations.length;
-  cxApi.setChosenVariation(variation, experiment);
+    variation = Math.floor(Math.random() * 1000000000) % pageVariations.length;
+    cxApi.setChosenVariation(variation, experiment);
 }
 
 pageVariations[variation]();
