c79b3f383e90ea6ba4b4f4c164f2f7877f4224d9
apeckham <apeckham@gmail.com>
2014-08-20 11:57:07 -0700

    start 6 experiments on autolink colors

--- experiments.js
+++ experiments.js
@@ -1,12 +1,26 @@
 var pageVariations = [
     function () {
+        $(".autolink").each(function () {
+            $(this).replaceWith($(this).text());
+        });
     },
     function () {
-        $(document.body).addClass('variation1');
+    },
+    function () {
+        $('.autolink').css({color: '#FFC745'});
+    },
+    function () {
+        $('.autolink').css({color: '#FFC745', 'text-decoration': 'underline'});
+    },
+    function () {
+        $('.autolink').css({'text-decoration': 'underline'});
+    },
+    function () {
+        $('.autolink').css({'background-color': '#FFC745', 'border-radius': '5px', padding: '3px', 'color': '#333'});
     }
 ];
 
-var experiment = 'A6baa2IYRrWkqeVgndseyA';
+var experiment = '9I2nDfeoTBi0qLE89paAZA';
 var variation = cxApi.getChosenVariation(experiment);
 if (variation === undefined) {
     variation = Math.floor(Math.random() * 1000000000) % pageVariations.length;
@@ -14,3 +28,6 @@ if (variation === undefined) {
 }
 
 pageVariations[variation]();
+
+window._gaq = window._gaq || [];
+window._gaq.push(['_trackEvent', 'Experiments', 'variation', experiment, variation, true]);
