97de41f35c64a68525939a3ffdbb404d8df98f4a
Aaron <apeckham@gmail.com>
2014-08-20 10:52:41 -0700

    Update experiments.js

---
+++ experiments.js
@@ -0,0 +1,16 @@
+var pageVariations = [
+  function() {},
+  function() {
+    var newClass = document.body.getAttribute('class') + ' variation1';
+    document.body.setAttribute('class', newClass);
+  }
+];
+
+var experiment = 'A6baa2IYRrWkqeVgndseyA';
+var variation = cxApi.getChosenVariation(experiment);
+if (!variation) {
+  var variation = Math.floor(Math.random() * 1000000000) % pageVariations.length;
+  cxApi.setChosenVariation(variation, experiment);
+}
+
+pageVariations[variation]();
