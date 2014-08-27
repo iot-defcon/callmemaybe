a5bbd9270b401ac8f903b70c9fbfb56403db8bdf
apeckham <apeckham@gmail.com>
2014-08-27 16:09:21 -0700

    how does pageviews per session change if we remove links from the top navigation?

--- experiments.js
+++ experiments.js
@@ -1,40 +1,48 @@
 var pageVariations = [
     function () {
-        $(".autolink").each(function () {
-            $(this).replaceWith($(this).text());
-        });
     },
     function () {
+        $("#nav1 a[href='/top_words.php']").parent("li").remove();
     },
     function () {
-        $('.autolink').css({color: '#FFC745'});
+        $("#nav1 a[href='/']").parent("li").remove();
     },
     function () {
-        $('.autolink').css({color: '#FFC745', 'text-decoration': 'underline'});
+        $("#nav1 a[href='/favorites.php']").parent("li").remove();
     },
     function () {
-        $('.autolink').css({'text-decoration': 'underline'});
+        $("#nav1 a[href='/popular.php?character=A']").parent("li").remove();
     },
     function () {
-        $('.autolink').css({'background-color': '#FFC745', 'border-radius': '5px', padding: '3px', 'color': '#333'});
+        $("#nav1 a[href='/game.php']").parent("li").remove();
     },
     function () {
-        $('.autolink').css({'background-color': 'lightgrey', 'border-radius': '5px', padding: '3px', 'color': 'white'});
+        $("#nav1 a[href='/thesaurus.php']").parent("li").remove();
+    },
+    function () {
+        $("#nav1 a[href='/name.php']").parent("li").remove();
+    },
+    function () {
+        $("#nav1 a[href='/products.php']").parent("li").remove();
+    },
+    function () {
+        $("#nav1 a[href='/add.php']").parent("li").remove();
+    },
+    function () {
+        $("#nav1 a[href='/users.php']").parent("li").remove();
     }
 ];
 
-if (document.location.pathname == '/') {
-    var experiment = '9I2nDfeoTBi0qLE89paAZA';
-    var variation = cxApi.getChosenVariation(experiment);
-    if (variation != cxApi.NOT_PARTICIPATING) {
-        if (variation == cxApi.NO_CHOSEN_VARIATION) {
-            variation = Math.floor(Math.random() * 1000000000) % pageVariations.length;
-            cxApi.setChosenVariation(variation, experiment);
-        }
-
-        pageVariations[variation]();
-
-        window._gaq = window._gaq || [];
-        window._gaq.push(['_trackEvent', 'Experiments', 'variation', experiment, variation, true]);
+var experiment = 'xYsqL0doSUygdg-yCH17-w';
+var variation = cxApi.getChosenVariation(experiment);
+if (variation != cxApi.NOT_PARTICIPATING) {
+    if (variation == cxApi.NO_CHOSEN_VARIATION) {
+        variation = Math.floor(Math.random() * 1000000000) % pageVariations.length;
+        cxApi.setChosenVariation(variation, experiment);
     }
+
+    pageVariations[variation]();
+
+    window._gaq = window._gaq || [];
+    window._gaq.push(['_trackEvent', 'Experiments', 'variation', experiment, variation, true]);
 }
